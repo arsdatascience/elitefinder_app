73d9458a7d65f0ab9bdbfb3f28d2089797ee2e17
arsdatascience <arsdatascience@gmail.com>
2026-02-03 21:34:27 -0300

    fix: add missingsqlalchemy types Float and Boolean to db_models.py

--- elitefinder-painel/ai_service/models/db_models.py
+++ elitefinder-painel/ai_service/models/db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Float, Boolean
 from sqlalchemy.sql import func
 from core.database import Base
 from datetime import datetime
